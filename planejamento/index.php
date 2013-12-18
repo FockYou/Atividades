11e1e7d18dd22add4314d61ede66a179485f2a80
FockYou <lgoodgloves@hotmail.com>
2013-12-18 12:06:04 -0200

    Create index.php

---
+++ planejamento/index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<title> Planejamento </title>
+</head>
+<form action="programa.php" method="post">
+	<h1> financeiro </h1>
+	
+
+	<label>DataI: <label>&nbsp;&nbsp;
+	 <input type="text" name="dataInicial"/> <br/>
+	
+	<label>DataF: <label>&nbsp;
+	<input type="text" name="dataFinal"/>
+
+<br/><br/>
+	
+	<input type="submit" value="Enviar" />
+</form>
+
+</html>
+
+
