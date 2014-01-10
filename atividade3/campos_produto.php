9be57763dd0916fa440b729ac326b70b79110731
FockYou <lgoodgloves@hotmail.com>
2014-01-10 12:50:30 -0200

    Create campos_produto.php

---
+++ atividade3/campos_produto.php
@@ -0,0 +1,30 @@
+<fieldset>
+<label>Nome do produto:</label><input type="text" name="nome" />
+</fieldset>        
+        <br/>
+        <br/>
+        <fieldset>
+        <input type="radio" name="cor" value="v" /><label>Verde</label>
+        <input type="radio" name="cor" value="a"  /><label>Azul</label><br/>
+        <input type="radio" name="cor" value="p" checked="checked"/><label>Preto</label><br/>
+        </fieldset>
+         <br/>
+        <fieldset>
+        <input type="checkbox" value="sim" name="novoproduto" > 
+        <label>produto novo</label><br/>
+        </fieldset>
+        <br/>
+        <fieldset>
+            <label>Prazo de entrega</label>
+            <select name="prazo">
+            <option value="5 Dias">5 Dias</option>
+            <option value="10 Dias">10 Dias</option>
+            <option value="15 Dias">15 Dias</option>
+            </select>
+        </fieldset>
+        <br/>
+         <fieldset>
+        <label>Descrição</label>
+        <textarea rows="4" cols="50" name="descricao"></textarea>
+         </fieldset>
+            
