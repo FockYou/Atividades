424c8321b2b8fdc84b56e65cee39d230cf25b4bc
FockYou <lgoodgloves@hotmail.com>
2014-02-07 10:58:53 -0200

    Create index.php

---
+++ avaliacao5/index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <title>Loja de Instrumentos</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        
+    </head>
+    <body>
+        <header id="topo">
+              <h1>Loja de Instrumentos</h1>
+        </header>
+        <fieldset>
+           <strong>Violino Michal</strong><br/>
+            <img src="img/violino.jpg"/></a><br/><i>De: R$ 458,85</i><br/><strong>Por: R$ 398,60</strong>
+            <a href="infoViolino.html" target=_blanc ><b>Mais informa&ccedil;&atilde;o</b></a><br/>
+        </fieldset>
+        
+         <fieldset>
+           <strong>Berinbau </strong><br/>
+            <img src="img/berinbau.jpg"/></a><br/><i>De: R$ 420,90</i><br/><strong>Por: R$ 350,40</strong>
+            <a href="infoBerinbau.html" target=_blanc >Mais informa&ccedil;&atilde;o</a><br/>
+        </fieldset>
+         
+        <fieldset>
+           <strong>Viol&atilde;o Yamaha El&eacute;trico</strong><br/>
+            <img src="img/violao.jpg"/></a><br/><i>De: R$ 1.466,21</i><br/><strong>Por: R$ 946,00</strong>
+            <a href="infoViolao.html" target=_blanc >Mais informa&ccedil;&atilde;o</a><br/>
+        </fieldset>
+        
+        <fieldset>
+           <strong>V-Piano Grand</strong><br/>
+            <img src="img/Piano.jpg"/></a><br/><i>De: R$ 4.699,00</i><br/><strong>Por: R$ 3.994,15</strong>
+            <a href="infoPiano.html" target=_blanc >Mais informa&ccedil;&atilde;o</a><br/>
+        </fieldset>
+        
+        
+        <footer>
+        <address><img src="img/images.jpg"> Lucas Goodgloves|<a href="https://www.facebook.com/Luc4s.GooD">Contato</a> </address>
+    </body>
+</html>
